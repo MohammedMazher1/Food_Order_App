0e432129f5dd005f1791157408006bb5014d55cd
MohammedMazher1 <muhmmed711300411@gmail.com>
2024-03-11 14:50:56 +0300

    add meals sections and styles

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from "./MealsSummary";
+import AvilableMeals from "./AvaiableMeals";
+import { Fragment } from "react";
+import AvailableMeals from "./AvaiableMeals";
+
+const Meals = () => {
+    return <Fragment>
+        <MealsSummary />
+        <AvailableMeals />
+    </Fragment>
+}
+
+export default Meals 
